c566b60d8f4329ae96a1e5f04d8c03e1cbd0e4a6
Arnaud Boudou <skuld@me.com>
2013-05-15 13:01:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+TinyRover
+=========
+
+TinyRover is an ATtiny85 based obstacle avoiding rover, programmed with Arduino IDE. The goal of this rover is to use few items and signals.
+
+* [Youtube video of TinyRover in action](http://www.youtube.com/watch?v=7z_jytPrwxs)
+
+
+Requirements
+------------
+
+Tiny Rover use few items:
+
+* An [ATTiny85](http://www.atmel.com/devices/attiny85.aspx) microcontroller which will be the “brain” of the rover.
+* A [Sharp GP2Y0A21YK0F](http://www.adafruit.com/product/164) IR distance sensor, which will be the “eyes” of the rover.
+* A [LD293D](http://www.adafruit.com/products/807) motor driver, which will be the “nerves” of the rover.
+* Two DC motors which will be the “muscles” of the rover
+* A [L78S05](http://www.st.com/web/catalog/sense_power/FM142/CL1015/SC315/PF63345) voltage regulator to power all of the above.
+
+We need to use as few items as possible because ATtiny85 provides only five I/O : four of then will be used as digital outputs to control L293D motor driver, and the last one will be used as analog input to be used with IR distance sensor.
+
+You can find all the schematics into “doc/TinyRover.fzz”, to open with [Fritzing](http://fritzing.org/).
+
+To program it, you'll have to use [Arduino](http://arduino.cc) IDE and upload to the ATtiny85 “TinyRover.ino” sketch. If you need info how upload sketches to ATtiny85 microcontrollers, you may refer to [http://hlt.media.mit.edu/?p=1695](http://hlt.media.mit.edu/?p=1695)
+
+How does it work
+----------------
+
+The algorithm of TinyRover is pretty simple:
+
+1. The rover goes forward until an obstacle is below 15 cm.
+2. Then the rover goes backward until the obstacle is at least at 25cm.
+3. Then the rover randomly turn left or right for 2 to 4 seconds
+4. Go to step 1
+
+The live assembly
+-----------------
+
+The frame of the rover is really too big, but I just used [MovingRaspi](https://github.com/aboudou/movingraspi)'s frame.
+
+![Live assembly](/imgs/TinyRover.jpg "Live assembly")
---
+++ TinyRover.ino
@@ -0,0 +1,100 @@
+// Motor 1 (right) forward pin
+#define MOTOR1FWDPIN 3
+// Motor 1 (right) reverse pin
+#define MOTOR1RVSPIN 4
+// Motor 2 (left) forward pin
+#define MOTOR2FWDPIN 0
+// Motor 2 (left) reverse pin
+#define MOTOR2RVSPIN 1
+// IR sensor pin
+#define IRPIN 2
+
+// Make the rover going forward
+void forwardRover() {
+  stopMotor();
+  digitalWrite(MOTOR1FWDPIN, HIGH);
+  digitalWrite(MOTOR2RVSPIN, LOW);
+  digitalWrite(MOTOR2FWDPIN, HIGH);
+  digitalWrite(MOTOR2RVSPIN, LOW);
+}
+
+// Make the rover going reverse
+void reverseRover() {
+  stopMotor();
+  digitalWrite(MOTOR1FWDPIN, LOW);
+  digitalWrite(MOTOR1RVSPIN, HIGH);
+  digitalWrite(MOTOR2FWDPIN, LOW);
+  digitalWrite(MOTOR2RVSPIN, HIGH);
+}
+
+// Make the rover turning right
+void turnRight() {
+  stopMotor();
+  digitalWrite(MOTOR1FWDPIN, LOW);
+  digitalWrite(MOTOR1RVSPIN, HIGH);
+  digitalWrite(MOTOR2FWDPIN, HIGH);
+  digitalWrite(MOTOR2RVSPIN, LOW);
+}
+
+// Make the rover turning left
+void turnLeft() {
+  stopMotor();
+  digitalWrite(MOTOR1FWDPIN, HIGH);
+  digitalWrite(MOTOR1RVSPIN, LOW);
+  digitalWrite(MOTOR2FWDPIN, LOW);
+  digitalWrite(MOTOR2RVSPIN, HIGH);
+}
+
+void stopMotor() {
+  digitalWrite(MOTOR1FWDPIN, LOW);
+  digitalWrite(MOTOR1RVSPIN, LOW);
+  digitalWrite(MOTOR2FWDPIN, LOW);
+  digitalWrite(MOTOR2RVSPIN, LOW);
+}
+  
+
+// Get distance (cm) measured by IR sensor
+float getDistance() {
+  // value from sensor * (5/1024) - if running 3.3.volts then change 5 to 3.3
+  float volts = (float)analogRead(IRPIN)*0.0048828125;   
+  return 65.0 * pow(volts, -1.10) / 2.55;
+}  
+
+void setup() {
+  // Initialize random generator
+  randomSeed(analogRead(IRPIN));
+  
+  // Initialize pins
+  pinMode(MOTOR1FWDPIN, OUTPUT);
+  pinMode(MOTOR1RVSPIN, OUTPUT);
+  pinMode(MOTOR2FWDPIN, OUTPUT);
+  pinMode(MOTOR2RVSPIN, OUTPUT);
+
+  // Start rover
+  forwardRover();
+}
+
+void loop() {
+  // If we are too close from an obstacle (i.e. less than 15 cm)
+  if (getDistance() < 15.0) {
+    // Reverse rover until we are at about 25 cm from the obstacle
+    reverseRover();
+    
+    while (getDistance() < 25.0) {
+      // Do nothing, just wait for the correct distance
+    }
+
+    // We randomly turn the rover to left or right, for a random amount of time (between 2 and 4 seconds)
+    if (random(150) < 75) {
+      turnLeft();
+    } else {
+      turnRight();
+    }
+    
+    //delay(4000);
+    delay(random(2000,4000));
+    
+    // We make the rover going forward again
+    forwardRover();
+  }
+}
